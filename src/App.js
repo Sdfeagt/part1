bf25e49de2b8241b6de0d60762f5982b652e4a55
Sdfeagt <rafalciechanski15@gmail.com>
2022-06-20 15:35:45 +0300

    App.js changed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
+const Header = (props) =>{
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) =>{
+  return(
+    <div>
+      <p>
+        {props.name} {props.no}
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <div>
+      <p>
+        Number of exercises {props.ex1 + props.ex2 + props.ex3}
+      </p>
+    </div>
+  )
+}
+
+
+
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +40,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}/>
+      <Content name={part1} no={exercises1}/>
+      <Content name={part2} no={exercises2}/>
+      <Content name={part3} no={exercises3}/>
+      <Total ex1={exercises1} ex2 ={exercises2} ex3={exercises3}/>
     </div>
   )
 }
