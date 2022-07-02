03399ef1c988a021a514197097e2ad10d109180b
Rafal <rafalciechanski15@gmail.com>
2022-07-02 21:05:32 +0300

    1.7 and 1.8 done

--- 1.6-1.14/src/App.js
+++ 1.6-1.14/src/App.js
@@ -6,6 +6,39 @@ const Button = (props) => (
   </button>
 )
 
+const Statistics = (props) => {
+  console.log("In Stats!")
+
+  let all = props.good + props.neutral + props.bad
+  let avg = (props.good - props.bad)/all
+  let pos_pro = (props.good/all) * 100
+
+  if (all === 0){
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  else{
+
+  if (props.good === 0){
+    pos_pro = 0
+  }
+
+  return (
+    <div>
+    <p> good {props.good}</p>
+    <p>neutral {props.neutral}</p>
+    <p>bad {props.bad}</p>
+    <p>all {all}</p>
+    <p>average {avg}</p>
+    <p> positive {pos_pro}%</p>
+    </div>
+  )
+}
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -20,9 +53,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <h1>statistics</h1>
-     <p> good {good}</p>
-     <p>neutral {neutral}</p>
-     <p>bad {bad}</p>
+     <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
